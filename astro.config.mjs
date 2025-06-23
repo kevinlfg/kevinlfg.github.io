afe1e612a4baa39854e1286f41f15cf1051fccdc
kevinlfg <kevin.lyfoung@gmail.com>
2025-06-23 22:31:06 +0000

    change user

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site:'https://klyfoung.github.io',
+    site:'https://kevinlfg.github.io',
     base: 'astro-notes'
 });
